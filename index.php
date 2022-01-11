7acf4a031156361bc168c4f74537d103614f20f2
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-11 13:35:02 +0100

    En cours

--- index.php
+++ index.php
@@ -2,26 +2,27 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits);
+echo $fruits[1];
 
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age["Ben"]. " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach ($age  as $key =>  $value) {
+    echo "<br>Key=" . $key  . ", Value=" . $value ;
 }
 
 
-
+echo "<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+echo sort($colors);
 
 
 // Quatrieme ligne
